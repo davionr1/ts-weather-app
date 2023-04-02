96aa127a16bb2a449aafa2f18a19de98c767fc8b
Josh Wedekind <josh@halfnibble.com>
2023-04-02 13:37:17 -0700

    Adding TypeScript types and notes.

---
+++ client/src/components/LocationOption.ts
@@ -0,0 +1,10 @@
+interface LocationOption {
+    id: number;
+    city: string;
+    city_ascii: string;
+    admin_name: string;
+    iso3: string;
+    // TODO: Finish the other props, e.g. lat, lng
+}
+
+export default LocationOption;
